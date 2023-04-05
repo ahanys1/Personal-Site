ad63308e095600ed1ff914d537588632c1330632
ahanys1 <alexanderhanys@gmail.com>
2023-04-04 20:05:45 -0400

    folders setup

---
+++ app.js
@@ -0,0 +1,25 @@
+// imports
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+//static files
+app.use(express.static('public'));
+app.use("/css", express.static(__dirname + '/public/css'));
+app.use("/js", express.static(__dirname + '/public/js'));
+app.use("/img", express.static(__dirname + '/public/img'));
+app.use("icons", express.static(__dirname + "/public/icons"));
+
+//set views
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+//nav
+app.get('', (req,res) => {
+    res.render('index',{text: 'this is variable'});
+})
+
+
+//listen on port 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
